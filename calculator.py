41b3757bd7f7e67a0d3eda06d541a083938cbc62
Azgar88 <144307334+Azgar88@users.noreply.github.com>
2023-11-08 21:41:52 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,22 @@
-def calculator():
-    while True:
-        my_str=input("enter your option")
-        for i in my_str:
-            if i=="1":
-                a=int(input("enter number"))
-                b=int(input("enter number"))
-                print(a+b)
-            elif i=="2":
-                a=int(input("enter number"))
-                b=int(input("enter number"))
-                print(a-b)
-            elif i=="3":
-                a=int(input("enter number"))
-                b=int(input("enter number"))
-                print(a*b)
-            elif i=="4":
-                a=int(input("enter number"))
-                b=int(input("enter number"))
-                print(a/b)
-            else:
-                print("not a valid option")
-calculator()
+while True:
+    def add(x,y):
+        return x+y
+    def subtract(x,y):
+        return x-y
+    def multiply(x,y):
+        return x*y
+    def division(x,y):
+        return x/y
+    my_str=input("enter your option")
+    x=int(input("enter number"))
+    y=int(input("enter number"))
+    if my_str=="1":
+        print(add(x,y))
+    elif my_str=="2":
+        print(subtract(x,y))
+    elif my_str=="3":
+        print(multiply(x,y))
+    elif my_str=="4":
+        print(division(x,y))
+    else:
+        print("invalid option")
